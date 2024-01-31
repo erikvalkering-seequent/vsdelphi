e6d137f709f55ea7ffa854fcf51ba93948b30cfb
Erik Valkering <erik.valkering@bentley.com>
2024-01-31 10:37:35 +0100

    Add @vscode/windows-registry package

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,11 @@ const extensionConfig = {
             loader: 'ts-loader'
           }
         ]
-      }
+      },
+      {
+        test: /\.node$/,
+        use: 'node-loader',
+      },
     ]
   },
   devtool: 'nosources-source-map',
